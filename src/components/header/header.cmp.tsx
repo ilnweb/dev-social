2b3c5967e2d52552065d845063c7d8836f77be29
Todor Dimov <todordimov@Todors-MBP.attlocal.net>
2020-04-18 19:21:33 -0500

    nishto novo

--- src/components/header/header.cmp.tsx
+++ src/components/header/header.cmp.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 
 const Header  = ({name} :Props) => (
-  <div className='header-main'>Hello, my name is {name}!</div>
+  <div className='header-main'>Hello, {name}!</div>
 );
 
 export default Header;
