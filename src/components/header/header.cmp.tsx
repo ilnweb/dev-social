31ef7b8ccb8bb9175cca8a28daedee5565f41553
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-21 12:33:00 +0200

    updating header with avatar and style

--- src/components/header/header.cmp.tsx
+++ src/components/header/header.cmp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.scss';
-import { Typography, Button, Row, Col } from 'antd';
+import { Typography, Button, Row, Col, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.config';
 
@@ -10,24 +10,28 @@ const { Title } = Typography;
 interface Props {
   user: {
     displayName?: string
-  } | null
+    photoURL?: string
+  }
 }
 
 const Header: React.FC<Props> = ({ user }) => (
   <div className='header-main'>
     <Row>
-    <Col span={18}>
-      <Title level={3}>Hello, {user && user.displayName}</Title>
-    </Col>
-    <Col span={6}>
-      <Button>
-        <Link to='/sign-in'>Sign In</Link>
+      <Col span={6}>
+        <Avatar size="large" src={user.photoURL} />
+      </Col>
+      <Col span={12}>
+        <Title level={3}>Hello, {user && user.displayName}</Title>
+      </Col>
+      <Col span={6}>
+        <Button>
+          <Link to='/sign-in'>Sign In</Link>
+        </Button>
+        <Button onClick={() => auth.signOut()}>
+          Sign Out
       </Button>
-      <Button onClick={()=>auth.signOut()}>
-        Sign Out
-      </Button>
-    </Col>
-  </Row>
+      </Col>
+    </Row>
   </div>
 );
 
