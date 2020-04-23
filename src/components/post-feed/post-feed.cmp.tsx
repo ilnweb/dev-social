497f09b12586052ed3ff1e4afa5bf09792cb697c
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-23 10:48:24 +0200

    updating post feed card

--- src/components/post-feed/post-feed.cmp.tsx
+++ src/components/post-feed/post-feed.cmp.tsx
@@ -26,20 +26,20 @@ const PostFeed: React.FC<Posts> = ({ posts }) => {
             key={index}
             className="post_card"
             bordered={false}
-            style={{ width: '100%', marginBottom: '3rem', marginTop: index === 0 ? '3rem' : '0' }}
+            style={{marginTop: index === 0 ? '3rem' : '0' }}
             cover={post.postImg && <div className="postImg" style={{background: `url(${post.postImg})`}}></div>
             }
-            extra={<Typography.Title level={4}>{'3h'}</Typography.Title>}
+            extra={<p>{'3h'}</p>}
             actions={[
-              <HeartOutlined style={{fontSize:'1.5rem'}} key="like" />,
-              <CommentOutlined style={{fontSize:'1.5rem'}} key="comment" />,
-              <ShareAltOutlined style={{fontSize:'1.5rem'}} key="share" />
+              <HeartOutlined className="icon-standart" key="like" />,
+              <CommentOutlined className="icon-standart" key="comment" />,
+              <ShareAltOutlined className="icon-standart" key="share" />
             ]}
           >
             <Meta
               title={<Typography.Title style={{marginTop: 30}} level={3}>{post.userName}</Typography.Title>}
               avatar={<Avatar style={{marginTop: 30}} size={50} src={post.userPhoto}></Avatar>}
-              description={<Typography.Title style={{lineHeight: '26px', whiteSpace: 'pre-wrap', margin: '0 30px 30px 0', textAlign: 'left'}} level={4}>{post.postBody}</Typography.Title>}
+              description={<p className="post-body">{post.postBody}</p>}
             />
           </Card>
         )
