029ad5701e057501ccdc27012fabd6ca237b75c7
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-26 16:18:45 +0200

    updating layout post-feed

--- src/pages/home-page/home-page.cmp.tsx
+++ src/pages/home-page/home-page.cmp.tsx
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react';
+import React, { useContext } from 'react';
 import './home-page.scss';
 import { Row, Col } from 'antd';
 import PostFeed from '../../components/post-feed/post-feed.cmp';
@@ -10,11 +10,11 @@ const HomePage = observer(() => {
   return (
     <div className='home-page'>
       <Row>
-        <Col span={6}></Col>
-        <Col span={12}>
-          <PostFeed posts={postContext.posts.slice().reverse()}/>
+        <Col span={6} sm={2} xs={1} lg={4}></Col>
+        <Col span={12} xs={22} sm={20} md={20} lg={16} xl={16}>
+          <PostFeed posts={postContext.posts.slice().reverse()} />
         </Col>
-        <Col span={6}></Col>
+        <Col span={6} sm={2} xs={1} lg={4}></Col>
       </Row>
     </div>
   )
