ed69c4d1ec63d2b08b5d579830a3a03b383e1312
iliyan <iliyan.tsachev@ilnweb.com>
2020-04-26 13:48:17 +0200

    update white post

--- src/pages/write-post/write-post.cmp.tsx
+++ src/pages/write-post/write-post.cmp.tsx
@@ -47,10 +47,10 @@ const WritePost: React.FC<Props> = observer(({ user }) => {
     }
   }
 
-  const handleClose = (tagToRemove:string): void => {
+  const handleClose = (tagToRemove: string): void => {
     const tags = post.postTags.filter(item => item !== tagToRemove);
-    
-  console.log(tagToRemove);
+
+    console.log(tagToRemove);
     setPost({ ...post, postTags: [...tags] })
   }
 
@@ -60,13 +60,17 @@ const WritePost: React.FC<Props> = observer(({ user }) => {
     });
   };
 
-  const handleChange = (e: React.FormEvent<HTMLTextAreaElement> | React.FormEvent<HTMLInputElement>): void => {
-    const { value, name } = e.currentTarget;
-    setPost({ ...post, [name]: value });
+  const handleChange = (e: React.FormEvent<HTMLTextAreaElement>): void => {
+    const { value } = e.currentTarget;
+    setPost({ ...post, postText: value });
+  };
+
+  const handleChangeTag = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = e.currentTarget;
     setTag(value);
   };
 
-console.log(post.postTags);
+  console.log(post.postTags);
   return (
     <div className='user-profile'>
       <Typography.Title level={2}>Write Post</Typography.Title>
@@ -74,29 +78,29 @@ console.log(post.postTags);
         <Col span={6}></Col>
         <Col span={12} >
           <Form className="login-regester__Form sign-in-up flex-c">
-          <Space direction="vertical" size="middle" style={{ width: '100%' }}>
-            <TextArea
-              rows={6}
-              name="postText"
-              value={post.postText}
-              className="input-style"
-              placeholder="Text"
-              autoComplete="true"
-              onChange={handleChange}
-            />
-            {post.postTags && post.postTags.map(item => (<Tag color='#e16162' closable onClose={()=>handleClose(item)}
-            >#{item}</Tag>))}
-            <Input
-              name="tag"
-              value={tag}
-              className="input-style"
-              placeholder="Tags"
-              autoComplete="true"
-              onChange={handleChange}
-              onKeyPress={handleKeyPress}
-            />
+            <Space direction="vertical" size="middle" style={{ width: '100%' }}>
+              <TextArea
+                rows={6}
+                name="postText"
+                value={post.postText}
+                className="input-style"
+                placeholder="Text"
+                autoComplete="true"
+                onChange={handleChange}
+              />
+              {post.postTags && post.postTags.map(item => (<Tag color='#e16162' closable onClose={() => handleClose(item)}
+              >#{item}</Tag>))}
+              <Input
+                name="tag"
+                value={tag}
+                className="input-style"
+                placeholder="Tags"
+                autoComplete="true"
+                onChange={handleChangeTag}
+                onKeyPress={handleKeyPress}
+              />
               <UploadImage handleImage={handleImage} />
-              </Space>
+            </Space>
           </Form>
           <Button className="button primary block" size="large" type="primary" onClick={handleSubmit}>
             Submit Post
