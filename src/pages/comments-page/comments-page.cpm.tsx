aa63547a319899047e4e8d75bcc6f67089474dcd
iliyan <iliyan.tsachev@ilnweb.com>
2020-05-11 17:11:00 +0200

    creating the comments layout

--- src/pages/comments-page/comments-page.cpm.tsx
+++ src/pages/comments-page/comments-page.cpm.tsx
@@ -1,33 +1,54 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './comments-page.scss';
-import { Comment, Avatar, Form, Button, List, Input,Row, Col } from 'antd';
+import { Avatar, Form, Button, List, Input, Row, Col } from 'antd';
 import { observer } from 'mobx-react-lite';
 import { PostContext } from '../../mobX/post-feed/post-feed.context';
+import { UserContext } from '../../mobX/user/user.context';
 
-const CommentPage = observer(() => {
-  const postContext = useContext(PostContext);
+const { TextArea } = Input;
+
+type User = {
+  user:{
+   photoURL?:string
+  }
+}
+
+
+
+const CommentPage: React.FC = observer(() => {
+  const [comment, setComment] = useState({ commentText: '' });
+  const postContext = React.useContext(PostContext);
+  const userContext = useContext<User>(UserContext);
+
+  console.log(postContext.posts);
+
+  const handleChange = (e: React.FormEvent<HTMLTextAreaElement>): void => {
+    const { value } = e.currentTarget;
+    setComment({ commentText: value });
+  };
   return (
     <div className='comments-page'>
       <Row>
         <Col span={6} sm={2} xs={1} lg={4}></Col>
         <Col span={12} xs={22} sm={20} md={20} lg={16} xl={16}>
           <h1>Comments</h1>
-        <div>
-            {
-              // comments.length > 0 && <CommentList comments={comments} />
-            }
-        <Comment
-          avatar={
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
-            />
-          }
-          content={
-            'sadsadsa'
-          }
-        />
-      </div>
+          <div></div>
+          <div className='write-comment'>
+            <div>
+              <Avatar size={60} src={userContext.user.photoURL} /></div>
+            <div className="comment-right">
+              <TextArea
+                rows={6}
+                name="postText"
+                value={comment.commentText}
+                className="comment-textarea"
+                placeholder="Text"
+                autoComplete="true"
+                onChange={handleChange}
+              />
+              <Button>Add Comment</Button>
+            </div>
+          </div>
         </Col>
         <Col span={6} sm={2} xs={1} lg={4}></Col>
       </Row>
