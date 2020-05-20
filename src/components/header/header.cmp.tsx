69a044f529329f9869d753bcd4887157ba6f26dd
iliyan <iliyan.tsachev@ilnweb.com>
2020-05-20 17:51:25 +0200

    updating layout

--- src/components/header/header.cmp.tsx
+++ src/components/header/header.cmp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.scss';
-import { Typography, Row, Col, Avatar } from 'antd';
+import { Typography, Row, Col, Avatar, Space } from 'antd';
 import Button from 'antd/es/button';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.config';
@@ -16,22 +16,27 @@ interface Props {
 
 const Header: React.FC<Props> = ({ user }) => (
   <div className='header-main'>
-    <Row>
+    <Row justify='center'>
       <Col span={6}>
-        <Link to='/user-profile'>
-          <Avatar size="large" src={user ? user.photoURL : 'N/a'} />
-        </Link>
+        
       </Col>
       <Col span={12}>
         <Title level={3}>Hello, {user && user.displayName}</Title>
       </Col>
-      <Col span={6} flex={1}>
-        <Button>
-          <Link to='/sign-in'>Sign In</Link>
-        </Button>
-        <Button onClick={() => auth.signOut()}>
-          Sign Out
-      </Button>
+      <Col span={6}>
+        <Row justify='end' align='middle' >
+          <Space>
+            <Link to='/user-profile'>
+              <Avatar size="large" src={user ? user.photoURL : 'N/a'} />
+            </Link>
+            {user ? '' : <Button>
+              <Link to='/sign-in'>Sign In</Link>
+            </Button>}
+            <Button onClick={() => auth.signOut()}>
+              Sign Out
+          </Button>
+          </Space>
+        </Row>
       </Col>
     </Row>
   </div>
