1f258b888f5c58dcf79d6ce5d108b651e5b26590
iliyan <iliyan.tsachev@ilnweb.com>
2020-05-29 16:58:44 +0200

    user-page styles

--- src/pages/user-profile/user-profile.cmp.tsx
+++ src/pages/user-profile/user-profile.cmp.tsx
@@ -9,14 +9,18 @@ const UserProfile: React.FC<IUser> = observer(({ user }) => {
   return (
     <div className='user-profile'>
       <Row>
-        <Col span={6}></Col>
-        <Col span={12}>
-          <Avatar size={200} src={user ? user.photoURL : ''} />
+        <Col span={6} sm={2} xs={1} lg={6}></Col>
+        <Col span={6} xs={22} sm={20} md={20} lg={12} xl={12}>
           <Typography.Title level={1} style={{ color: 'white' }}>Your Profile</Typography.Title>
-          <Typography.Title level={2} style={{ color: 'white' }}>{user && user.displayName}</Typography.Title>
-          <Typography.Title level={4} style={{ color: 'white' }}>{user && user.email}</Typography.Title>
+          <div className="user-data">
+            <div><Avatar size={70} src={user ? user.photoURL : ''} /> </div>
+            <div className="user-info">
+              <Typography.Title level={2} style={{ color: 'white' }}>{user && user.displayName}</Typography.Title>
+              <Typography.Title level={4} style={{ color: 'white' }}>{user && user.email}</Typography.Title>
+            </div>
+          </div>
         </Col>
-        <Col span={6}></Col>
+        <Col span={6} sm={2} xs={1} lg={6}></Col>
       </Row>
     </div>
   )
