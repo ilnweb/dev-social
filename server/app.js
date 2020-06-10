69bbd74cdba64a51856dfbf06b1e6175dd653b41
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-10 12:12:28 +0200

    setting up server

--- server/app.js
+++ server/app.js
@@ -1,1 +1,32 @@
-const path = require('path')
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const multer = require('multer');
+// const feedRoutes = require('./routes/feed');
+// const authRoutes = require('./routes/auth');
+
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(bodyParser.json()); // application/json
+app.use(bodyParser.urlencoded({ extended: true })); // application/json
+app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+  })
+}
+
+mongoose.connect('mongodb+srv://iliyan:codemode8894@cluster0-s4kfe.mongodb.net/dev-social?retryWrites=true&w=majority', {useUnifiedTopology: true,useNewUrlParser: true});
+app.listen(port, err => {
+  if (err) throw err;
+  console.log('Server running on port' + port);
+});
