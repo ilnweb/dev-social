91117704062cb1e7e2fce64a58903d05b3514780
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-15 15:14:32 +0200

    headr change dropdown

--- client/src/components/header/header.cmp.tsx
+++ client/src/components/header/header.cmp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './header.scss';
-import { Typography, Row, Col, Avatar, Space } from 'antd';
+import { Typography, Row, Col, Avatar, Space, Dropdown, Menu } from 'antd';
 import Button from 'antd/es/button';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.config';
@@ -8,42 +8,67 @@ import { IUser } from '../../interfaces/interfaces';
 
 const { Title } = Typography;
 
-const Header: React.FC<IUser> = ({ user }) => (
-  <div className='header-main'>
-    <Row align='middle'>
-      <Col span={6} flex={1}>
-        <Link to='/'>
-          <h1 className='logo'>DEV</h1>
+const Header: React.FC<IUser> = ({ user }) => {
+  const menu = (
+    <Menu className="header-main-menu">
+      <Menu.Item>
+        <Link to='/user-profile'>
+          Profile
         </Link>
-      </Col>
-      <Col span={12}>
-        <Title level={3}>{user && `Hello, && ${user.displayName}`}</Title>
-      </Col>
-      <Col span={6}>
-        <Row justify='end' align='middle' >
-          <Space>
-            {user ?
-              <Link to='/user-profile'>
-                <Avatar size="large" style={{backgroundColor:'#e16162',fontSize:'1.4rem', fontWeight:500}} src={user ? user.photoURL : ''} >{user?.displayName?.split('')[0].toUpperCase()}</Avatar>
-              </Link>
-              : (
-                <Space>
-                  <Button>
-                    <Link to='/sign-in'>Sign In</Link>
-                  </Button>
-                  <Button>
-                    <Link to='/sign-up'>Sign Up</Link>
-                  </Button>
-                </Space>
-              )}
-            {user && <Button onClick={() => auth.signOut()}>
-              Sign Out
-          </Button>}
-          </Space>
-        </Row>
-      </Col>
-    </Row>
-  </div>
-);
+      </Menu.Item>
+      <Menu.Item>
+        <Link to='/user-profile'>
+          Write Post
+        </Link>
+      </Menu.Item>
+      <Menu.Item>
+        <Link to='/user-profile'>
+          Saved Posts
+        </Link>
+      </Menu.Item>
+      <hr></hr>
+      <Menu.Item>
+        <Link to='/user-profile'>
+          Sign Out
+        </Link>
+      </Menu.Item>
+    </Menu>
+  );
+  return (
+    <div className='header-main'>
+      <Row align='middle'>
+        <Col span={6} flex={1}>
+          <Link to='/'>
+            <h1 className='logo'>DEV</h1>
+          </Link>
+        </Col>
+        <Col span={12}>
+          <Title level={3}>{user && `Hello, ${user.displayName}`}</Title>
+        </Col>
+        <Col span={6}>
+          <Row justify='end' align='middle' >
+            <Space>
+              {user ?
+                <Dropdown overlay={menu} trigger={['click']} overlayClassName="header-main-dropdown" placement="bottomCenter">
+                  <Avatar size="large" style={{ backgroundColor: '#e16162', fontSize: '1.4rem', fontWeight: 500,cursor:"pointer" }} src={user ? user.photoURL : ''} >{user?.displayName?.split('')[0].toUpperCase()}</Avatar>
+                </Dropdown>
+                : (
+                  <Space>
+                    <Button>
+                      <Link to='/sign-in'>Sign In</Link>
+                    </Button>
+                    <Button>
+                      <Link to='/sign-up'>Sign Up</Link>
+                    </Button>
+                  </Space>
+                )}
+
+            </Space>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  )
+};
 
 export default Header;
