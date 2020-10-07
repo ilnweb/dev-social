c1252b59be947cd435f2d3933845618f2b08eaa5
iliyan <iliyan.tsachev@ilnweb.com>
2020-10-07 11:57:04 +0200

    creating the user reading list and user posts pages

---
+++ client/src/pages/reading-list/reading-list.cmp.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAllPosts } from '../../redux/posts/post-selectors';
+import './reading-list.scss';
+import { Row, Col } from 'antd';
+import PostFeed from '../../components/post-feed/post-feed.cmp';
+
+const ReadingList: React.FC = () => {
+  const posts = useSelector(selectAllPosts);
+  return (
+    <div className='home-page'>
+      <Row>
+        <Col span={6} sm={0} md={2} lg={4} xl={6}></Col>
+        <Col span={24} sm={24} md={20} lg={16} xl={12}>
+          <PostFeed posts={posts} />
+        </Col>
+        <Col span={6} sm={0} md={2} lg={4} xl={6}></Col>
+      </Row>
+    </div>
+  )
+};
+
+export default ReadingList;
